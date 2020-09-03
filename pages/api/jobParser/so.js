8942a9fa19480dcd0679de4af9be8c580825be45
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-09-03 15:33:49 +0200

    req log

--- pages/api/jobParser/so.js
+++ pages/api/jobParser/so.js
@@ -8,16 +8,19 @@ let parser = new Parser({
 const feedSource = "https://stackoverflow.com/jobs/feed?r=true"
 
 export default async (req, res) => {
-  if (req.method === "POST") {
-    // Process a POST request
-  } else {
-    // Handle any other HTTP method
+  if (req.method === "GET") {
+    console.log(req)
     let feed = await parser.parseURL(feedSource)
     for (let i = 0; i < 25; i++) {
       console.log(feed.items[i].title + ":" + feed.items[i].link)
     }
+    // Everything is Okay
     res.statusCode = 200
     res.setHeader("Content-Type", "application/json")
     res.end(JSON.stringify({ status: "OK" }))
+  } else {
+    // Handle any other HTTP method
+    res.setHeader("Allow", ["GET"])
+    res.status(405).end(`Method ${method} Not Allowed`)
   }
 }
