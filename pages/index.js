a3c346d12598d6f933b28fa373769448c26a4485
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-09-03 13:18:54 +0200

    test2

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,15 @@ export async function getStaticProps(ctx) {
     ? `https://${process.env.VERCEL_URL}`
     : `http://localhost:3000`
 
-  const categories = await fetch(`${url}/api`)
+  const categories = await fetch(`${url}/api`, {
+    method: "GET",
+    headers: {
+      // update with your user-agent
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+      Accept: "application/json; charset=UTF-8",
+    },
+  })
   const data = await categories.json()
   return {
     props: {
