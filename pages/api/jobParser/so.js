81d9ca6f3e895eb7268cef7ea8d7a91cb580a53c
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-09-03 15:50:18 +0200

    nog een test

--- pages/api/jobParser/so.js
+++ pages/api/jobParser/so.js
@@ -9,15 +9,18 @@ const feedSource = "https://stackoverflow.com/jobs/feed?r=true"
 
 export default async (req, res) => {
   if (req.method === "GET") {
-    console.log(req.headers)
-    let feed = await parser.parseURL(feedSource)
-    for (let i = 0; i < 5; i++) {
-      console.log(feed.items[i].title + ":" + feed.items[i].link)
+    if (req.headers.authorization !== "Basic YXV0aDo5VjhMcSpWcjBONVM=") {
+      res.status(403).end(`Not Allowed`)
+    } else {
+      let feed = await parser.parseURL(feedSource)
+      for (let i = 0; i < 5; i++) {
+        console.log(feed.items[i].title + ":" + feed.items[i].link)
+      }
+      // Everything is Okay
+      res.statusCode = 200
+      res.setHeader("Content-Type", "application/json")
+      res.end(JSON.stringify({ status: "OK" }))
     }
-    // Everything is Okay
-    res.statusCode = 200
-    res.setHeader("Content-Type", "application/json")
-    res.end(JSON.stringify({ status: "OK" }))
   } else {
     // Handle any other HTTP method
     res.setHeader("Allow", ["GET"])
