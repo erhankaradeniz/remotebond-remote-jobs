66a373da5f13bc22a58f49178f3f345f11f73584
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-09-03 15:53:51 +0200

    nog een test

--- pages/api/jobParser/so.js
+++ pages/api/jobParser/so.js
@@ -8,22 +8,21 @@ let parser = new Parser({
 const feedSource = "https://stackoverflow.com/jobs/feed?r=true"
 
 export default async (req, res) => {
-  if (req.method === "GET") {
-    if (req.headers.authorization !== "Basic YXV0aDo5VjhMcSpWcjBONVM=") {
-      res.status(403).end(`Not Allowed`)
-    } else {
-      let feed = await parser.parseURL(feedSource)
-      for (let i = 0; i < 5; i++) {
-        console.log(feed.items[i].title + ":" + feed.items[i].link)
-      }
-      // Everything is Okay
-      res.statusCode = 200
-      res.setHeader("Content-Type", "application/json")
-      res.end(JSON.stringify({ status: "OK" }))
+  if (
+    req.method === "GET" &&
+    req.headers.authorization === "Basic YXV0aDo5VjhMcSpWcjBONVM="
+  ) {
+    let feed = await parser.parseURL(feedSource)
+    for (let i = 0; i < 5; i++) {
+      console.log(feed.items[i].title + ":" + feed.items[i].link)
     }
+    // Everything is Okay
+    res.statusCode = 200
+    res.setHeader("Content-Type", "application/json")
+    res.end(JSON.stringify({ status: "OK" }))
   } else {
     // Handle any other HTTP method
     res.setHeader("Allow", ["GET"])
-    res.status(405).end(`Method ${method} Not Allowed`)
+    res.status(405).end(`Not Allowed`)
   }
 }
