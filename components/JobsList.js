7e107be71d820bcf74181e4880810a377832cf9c
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-09-23 17:04:56 +0200

    add customer support feed source from wwr

--- components/JobsList.js
+++ components/JobsList.js
@@ -40,7 +40,7 @@ const JobsList = ({
                     <div className="px-4 py-4 sm:px-6">
                       <div className="flex items-center justify-between">
                         <div className="text-sm leading-5 font-medium text-blue-600 truncate">
-                          {title} - <span className="text-red-500">{id}</span>
+                          {title}
                         </div>
                         <div className="ml-2 flex-shrink-0 flex">
                           <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
