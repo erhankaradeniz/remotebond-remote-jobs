3eb958c5940b5575fda80fe3856f13c0cf61b0fd
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-10-15 09:52:04 +0200

    test

--- pages/u/[username].js
+++ pages/u/[username].js
@@ -1,11 +1,11 @@
 import React from "react"
+import { useRouter } from "next/router"
 
 import getAllUsers, { getUserByUsername } from "../../lib/user"
 
 export async function getStaticPaths() {
   const users = await getAllUsers()
   if (users.length) {
-    console.log(users)
     return {
       paths: users.map((user) => {
         return {
@@ -36,24 +36,45 @@ export async function getStaticProps(ctx) {
 }
 
 const UserProfilePage = ({ user }) => {
-  return (
-    <div className="flex flex-col flex-1">
-      <div className="relative overflow-hidden bg-black">
-        <div className="max-w-screen-xl mx-auto py-16 px-4 sm:px-6 lg:py-12 lg:px-8">
-          <div>
-            <h2 className="text-center text-3xl leading-9 font-extrabold text-white">
-              {user.username}
-            </h2>
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return (
+      <div className="max-w-screen-xl mx-auto py-10 px-4 sm:px-6">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+  if (!user) {
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+        </Head>
+        <DefaultErrorPage statusCode={404} />
+      </>
+    )
+  } else {
+    return (
+      <div className="flex flex-col flex-1">
+        <div className="relative overflow-hidden bg-black">
+          <div className="max-w-screen-xl mx-auto py-16 px-4 sm:px-6 lg:py-12 lg:px-8">
+            <div>
+              <h2 className="text-center text-3xl leading-9 font-extrabold text-white">
+                {user.username}
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+          <div className="max-w-md w-full">
+            <p>Profile page is coming with the next update. Stay tuned!</p>
           </div>
         </div>
       </div>
-      <div className="flex flex-1 items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full">
-          <p>Profile page is coming with the next update. Stay tuned!</p>
-        </div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default UserProfilePage
