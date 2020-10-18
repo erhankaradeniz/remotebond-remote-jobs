22ab965cc6bbe3c8d5d6322e08e3d708bb427c8d
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-10-18 15:54:17 +0200

    remove old header component and replace with new one

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,12 @@
 import React from "react"
 
 import Header from "./Header"
-import HeaderNew from "./HeaderNew"
 import Footer from "./Footer"
 
 const Layout = ({ children }) => {
   return (
     <>
-      {/* <Header /> */}
-      <HeaderNew />
+      <Header />
       <main className="flex flex-col flex-grow">{children}</main>
       <Footer />
     </>
