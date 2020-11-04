501d7da6cc77c9cd761bc457e2be4bcd602d5609
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-11-04 15:18:49 +0100

    add remote companies to header and create page, also add remote companies to sitemap

--- components/Header.js
+++ components/Header.js
@@ -154,6 +154,18 @@ const HeaderNew = () => {
                   Remote jobs
                 </a>
               </Link>
+              <Link href={`/remote-companies`} as={`/remote-companies`}>
+                <a
+                  className={`${
+                    currentPath === "/remote-companies"
+                      ? "ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-blue-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blue-700 transition duration-150 ease-in-out"
+                      : "ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                  }`}
+                  title="Remote companies on Remotebond"
+                >
+                  Remote companies
+                </a>
+              </Link>
               <Link href={`/forum`} as={`/forum`}>
                 <a
                   className={`${
@@ -301,6 +313,18 @@ const HeaderNew = () => {
               }`}
             >
               Remote jobs
+            </a>
+          </Link>
+          <Link href={`/remote-companies`} as={`/remote-companies`}>
+            <a
+              title="Remote companies on Remotebond"
+              className={`${
+                currentPath === "/remote-companies"
+                  ? "block pl-3 pr-4 py-2 border-l-4 border-blue-500 text-base font-medium text-blue-700 bg-blue-50 focus:outline-none focus:text-blue-800 focus:bg-blue-100 focus:border-blue-700 transition duration-150 ease-in-out"
+                  : "block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
+              }`}
+            >
+              Remote companies
             </a>
           </Link>
           <Link href={`/forum`} as={`/forum`}>
---
+++ pages/remote-companies/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const RemoteCompaniesIndexPage = () => {
+  return (
+    <div>
+      <p>Remote companies index </p>
+    </div>
+  )
+}
+
+export default RemoteCompaniesIndexPage
