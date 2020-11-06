2b8207f75862d6306556e6275850910796398c22
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-11-06 14:50:16 +0100

    add companies to sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,8 +1,12 @@
 import React from "react"
 
 import getAllJobs from "../lib/jobs"
+import { getAllCompaniesOnly } from "../lib/company"
 
-const createSitemap = (jobs) => `<?xml version="1.0" encoding="UTF-8"?>
+const createSitemap = (
+  jobs,
+  companies
+) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
             <loc>${`https://remotebond.com`}</loc>
@@ -84,15 +88,33 @@ const createSitemap = (jobs) => `<?xml version="1.0" encoding="UTF-8"?>
             })
             .join("")
         }
+        ${
+          companies.length &&
+          companies
+            .map((company) => {
+              const {
+                company: { data: companyData },
+              } = company
+              return `
+                    <url>
+                        <loc>${`https://remotebond.com/remote-companies/${companyData.slug}`}</loc>
+                        <changefreq>hourly</changefreq>
+                        <priority>0.9</priority>
+                        <lastmod>${new Date().toISOString()}</lastmod>
+                    </url>
+                `
+            })
+            .join("")
+        }
     </urlset>
     `
 
 class Sitemap extends React.Component {
   static async getInitialProps({ res }) {
     const jobs = await getAllJobs()
-
+    const companies = await getAllCompaniesOnly()
     res.setHeader("Content-Type", "text/xml")
-    res.write(createSitemap(jobs))
+    res.write(createSitemap(jobs, companies))
     res.end()
   }
 }
