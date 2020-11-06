7859da06d5684dafa819959d47f858e303b603b6
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-11-06 15:08:56 +0100

    only show jobs when available

--- pages/remote-companies/[companySlug].js
+++ pages/remote-companies/[companySlug].js
@@ -53,61 +53,85 @@ const RemoteCompaniesIndexPage = (props) => {
     month: "long",
   }
 
-  return (
-    <>
-      <NextSeo
-        title={`${company.name} hiring remotely`}
-        description={`${company.name} is hiring remotely. ${
-          jobs.length
-        } remote jobs open in ${date.toLocaleDateString("en-EN", dateOptions)}`}
-        canonical={`https://remotebond.com/remote-companies/${company.slug}`}
-        openGraph={{
-          url: `https://remotebond.com/remote-companies`,
-          title: `${company.name} hiring remotely`,
-          description: `${company.name} is hiring remotely. ${
+  if (router.isFallback) {
+    return (
+      <div className="max-w-screen-xl mx-auto py-10 px-4 sm:px-6">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+  if (!company) {
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+        </Head>
+        <DefaultErrorPage statusCode={404} />
+      </>
+    )
+  } else {
+    return (
+      <>
+        <NextSeo
+          title={`${company.name} hiring remotely`}
+          description={`${company.name} is hiring remotely. ${
             jobs.length
           } remote jobs open in ${date.toLocaleDateString(
             "en-EN",
             dateOptions
-          )}`,
-        }}
-      />
-      <BreadcrumbJsonLd
-        itemListElements={[
-          {
-            position: 1,
-            name: "remotebond.com",
-            item: "https://remotebond.com",
-          },
-          {
-            position: 2,
-            name: "Remote Companies",
-            item: "https://remotebond.com/remote-companies",
-          },
-          {
-            position: 3,
-            name: `${company.name}`,
-          },
-        ]}
-      />
-      <div className="relative overflow-hidden bg-black">
-        <div className="max-w-screen-xl mx-auto text-center py-6 md:py-12 px-4 sm:px-6">
-          <h1 className="text-white font-black text-2xl md:text-4xl my-4">
-            {company.name}
-          </h1>
-          <h2 className="text-base md:text-xl text-rb-gray-4 w-3/4 mx-auto">
-            {company.name} is hiring remotely and currently has {jobs.length}{" "}
-            remote job {jobs.length === 1 ? "position" : "positions"} open in{" "}
-            {date.toLocaleDateString("en-EN", dateOptions)}
-          </h2>
+          )}`}
+          canonical={`https://remotebond.com/remote-companies/${company.slug}`}
+          openGraph={{
+            url: `https://remotebond.com/remote-companies`,
+            title: `${company.name} hiring remotely`,
+            description: `${company.name} is hiring remotely. ${
+              jobs.length
+            } remote jobs open in ${date.toLocaleDateString(
+              "en-EN",
+              dateOptions
+            )}`,
+          }}
+        />
+        <BreadcrumbJsonLd
+          itemListElements={[
+            {
+              position: 1,
+              name: "remotebond.com",
+              item: "https://remotebond.com",
+            },
+            {
+              position: 2,
+              name: "Remote Companies",
+              item: "https://remotebond.com/remote-companies",
+            },
+            {
+              position: 3,
+              name: `${company.name}`,
+            },
+          ]}
+        />
+        <div className="relative overflow-hidden bg-black">
+          <div className="max-w-screen-xl mx-auto text-center py-6 md:py-12 px-4 sm:px-6">
+            <h1 className="text-white font-black text-2xl md:text-4xl my-4">
+              {company.name}
+            </h1>
+            <h2 className="text-base md:text-xl text-rb-gray-4 w-3/4 mx-auto">
+              {company.name} is hiring remotely and currently has {jobs.length}{" "}
+              remote job {jobs.length === 1 ? "position" : "positions"} open in{" "}
+              {date.toLocaleDateString("en-EN", dateOptions)}
+            </h2>
+          </div>
         </div>
-      </div>
 
-      <div className="w-full max-w-screen-xl mx-auto py-10 px-4 sm:px-6">
-        <JobsList title={`Remote jobs at ${company.name}`} jobs={jobs} />
-      </div>
-    </>
-  )
+        <div className="w-full max-w-screen-xl mx-auto py-10 px-4 sm:px-6">
+          {jobs && jobs.length && (
+            <JobsList title={`Remote jobs at ${company.name}`} jobs={jobs} />
+          )}
+        </div>
+      </>
+    )
+  }
 }
 
 export default RemoteCompaniesIndexPage
