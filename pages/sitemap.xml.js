7b52a28c4fb7a93e4f599aa8e1000d49162219a9
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-11-11 17:55:47 +0100

    fix sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -6,7 +6,8 @@ import { getAllTags } from "../lib/tag"
 
 const createSitemap = (
   jobs,
-  companies
+  companies,
+  tags
 ) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
