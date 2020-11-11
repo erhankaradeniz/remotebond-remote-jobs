03f8ba92552a08250d16fb0f5015430356c97a46
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-11-11 16:04:10 +0100

    remove updateJObs api endpoint

--- pages/api/updateJobs.js
+++
@@ -1,9 +0,0 @@
-import { getAndUpdateAllJobs } from "../../lib/jobs"
-
-export default async (req, res) => {
-  const dbs = await getAndUpdateAllJobs()
-
-  res.statusCode = 200
-  res.setHeader("Content-Type", "application/json")
-  res.json({ status: "Success" })
-}
