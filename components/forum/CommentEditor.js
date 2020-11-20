c15ea289ee3a7e0eeb05a2cb6bec7d9ad7a4c802
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-11-20 16:20:26 +0100

    fix editor

--- components/forum/CommentEditor.js
+++ components/forum/CommentEditor.js
@@ -2,6 +2,8 @@ import React from "react"
 import dynamic from "next/dynamic"
 import { Controller } from "react-hook-form"
 
+import "react-quill/dist/quill.snow.css"
+
 const QuillNoSSRWrapper = dynamic(import("react-quill"), {
   ssr: false,
   loading: () => <p>Loading ...</p>,
