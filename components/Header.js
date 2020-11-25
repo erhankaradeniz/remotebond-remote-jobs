25eb39ed711ab7cf26c357331972582eef87675f
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-11-25 14:23:34 +0100

    dont prefetch hire-remotely

--- components/Header.js
+++ components/Header.js
@@ -286,7 +286,11 @@ const HeaderNew = () => {
               </Transition>
             </div>
             <span className="inline-flex rounded-md shadow-sm ml-3">
-              <Link href={`/hire-remotely`} as={`/hire-remotely`}>
+              <Link
+                href={`/hire-remotely`}
+                as={`/hire-remotely`}
+                prefetch={false}
+              >
                 <a className="inline-flex items-center px-4 py-3 border border-transparent text-base leading-4 font-bold rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 hover:text-white transition ease-in-out duration-150">
                   Post a job
                 </a>
