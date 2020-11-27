25eee4e1ce9093c70d785d22f37a23f02932e025
Erhan Karadeniz <erhan@erhankaradeniz.com>
2020-11-27 14:21:17 +0100

    add some x-headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  headers: [
+    {
+      key: "X-Frame-Options",
+      value: "DENY",
+    },
+    {
+      key: "X-Content-Type-Options",
+      value: "nosniff",
+    },
+  ],
   images: {
     loader: "cloudinary",
     path: "https://res.cloudinary.com/remotebond/image/upload/",
